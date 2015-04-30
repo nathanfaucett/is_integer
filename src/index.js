18714eea3a65101b1dc074b9dd15f1425038e4c3
Nathan Faucett <nathanfaucett@gmail.com>
2015-04-30 12:09:36 -0500

    updates

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 var isNumber = require("is_number");
 
 
-module.exports = function isInteger(obj) {
+module.exports = isInteger;
+
+
+function isInteger(obj) {
     return isNumber(obj) && obj % 1 === 0;
-};
+}
